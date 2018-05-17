27f0b83272fa0de09281799dc989aedb1f423a93
heweigeng1 <6318275@qq.com>
2018-05-17 18:09:52 +0800

    初始化

---
+++ LegoAbp.Zero/Users/Domain/User.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegoAbp.Zero.Users.Domain
+{
+    public class User : FullAuditedEntity<Guid>, IMayHaveTenant, IPassivable
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public bool IsActive { get; set; }
+        public int? TenantId { get; set; }
+    }
+}
