4802b59a508d219b27fc51f1928da787e0db3181
heweigeng <heweigeng@hotmail.com>
2019-03-23 00:52:15 +0800

    update

--- src/LegoAbp.EntityFrameworkCore/Migrations/20190322165119_init4.Designer.cs
+++ src/LegoAbp.EntityFrameworkCore/Migrations/20190322165119_init4.Designer.cs
@@ -4,14 +4,16 @@ using LegoAbp.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LegoAbp.Migrations
 {
     [DbContext(typeof(LegoAbpDbContext))]
-    partial class LegoAbpDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190322165119_init4")]
+    partial class init4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
